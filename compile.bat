ef6851b274f23adbe98bbe1d6c240c014a22a3aa
yangxin-stu <2192462422yx@gmail.com>
2023-08-22 21:08:30 +0800

    new files

--- compile.bat
+++ compile.bat
@@ -1,2 +1,2 @@
 cmake -G "Visual Studio 17 2022" -A x64 -S . -Bbuild
-cmake --build build --config Release
+cmake --build build
--- submit.bat
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "init"
-git push origin main --ignore-submodules
